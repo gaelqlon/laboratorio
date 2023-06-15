76a0152d8c7defe7d32fdc90dcc4562759884666
gael cardona <cardonagael11@gmail.com>
2023-06-15 10:38:15 -0600

    se creo el archivo main

--- laboratorio.py
+++ laboratorio.py
@@ -1,4 +1,23 @@
-n1=int(input("ingrese un numero"))
-n2=int(input("ingrese un numero"))
-elecion=0
-while elecion!=6:
+import math
+def suma(n1,n2):
+    return n1+n2
+def resta(n1,n2):
+    return n1-n2
+def multipliacion (n1,n2):
+    return n1*n2
+def divisiom(n1,n2):
+    return n1/n2
+def exponenciacion(n1,n2):
+    return n1-n2
+def raiz(n):
+    return math.sqrt(n)
+def suma_5numeros(n1,n2,n3,n4,n5):
+    return (n1+n2+n3+n4+n5)
+def residuo (n1,n2):
+    return (n1%n2)
+def quetzal_a_dolar (n):
+    return n/7.91
+def dolar_a_quetzal (n):
+    return n*7.91
+
+
